de5792f69b9afc339901d823870e34b28d91b89a
sanjanadilhara <sanjanadilhara2000@gmail.com>
2024-09-24 23:45:05 +0530

    nav off canvas

--- src/views/components/nav.ts
+++ src/views/components/nav.ts
@@ -1,9 +1,31 @@
 const mainNav = async () => {
     return `
+        <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+            <div class="offcanvas-header">
+                <h5 class="offcanvas-title" id="offcanvasExampleLabel">Menu</h5>
+                <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+                <div class="">
+                    <div class="fw-bold pb-2">
+                        Client manage
+                    </div>
+                    <div class="">
+                        <a href="/add-client" class="text-decoration-none text-dark d-block pb-1">
+                            Add Client
+                        </a>
+                        <a href="/client-list" class="text-decoration-none text-dark d-block pb-1">
+                            Client List
+                        </a>
+                    </div> 
+                </div>
+            </div>
+        </div>
+
       <div class="nav-bar">
         <div class="d-flex align-items-center gap-3">
             <div class="menu-icon">
-                <i class="fa-solid fa-bars"></i>
+                <i class="fa-solid fa-bars" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample"></i>
             </div>
             <div class="logo-text">HIJ</div>
         </div>

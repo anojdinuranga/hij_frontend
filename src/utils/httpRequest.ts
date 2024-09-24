a58fa5a2f3a8bb0c39c38d7b6419da61d58c58ae
sanjanadilhara <sanjanadilhara2000@gmail.com>
2024-09-24 22:04:07 +0530

    login integration

--- src/utils/httpRequest.ts
+++ src/utils/httpRequest.ts
@@ -3,6 +3,7 @@ import DefaultResponse from './DefaultResponse';
 import { ExtendedRequest } from '../config/types';
 import logger from '../config/logger';
 const getIP = require('ipware')().get_ip as any;
+import https from 'https'; 
 
 
 const post = async (url:string, data:any, token:string = "", req: ExtendedRequest|null = null) => {
@@ -11,14 +12,17 @@ const post = async (url:string, data:any, token:string = "", req: ExtendedReques
     if(req) {
         ipInfo = getIP(req);
     }
+    const agent = new https.Agent({
+        rejectUnauthorized: false,  // For testing purposes; don't use this in production
+        minVersion: 'TLSv1.2'       // Force the minimum TLS version
+      });
+      
     let ipAddress = (ipInfo) ? ipInfo.clientIp : "";
     let userAgent = ((req) ? req.headers['user-agent'] : "");
     return new Promise((resolve) => {
         axios.post(url, data, {
             headers: {
                 'Authorization': `Bearer ${token}`,
-                'x-forwarded-for': ipAddress,
-                'user-agent': userAgent
             }
         })
         .then(result => {
